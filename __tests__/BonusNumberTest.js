82136b6b08701b04ad1f46dda6a2777e202c98d5
rlaclghks123 <rlaclghks123@naver.com>
2023-12-03 17:46:49 +0900

    test: 보너스번호와 당첨번호 중복 테스트

--- __tests__/BonusNumberTest.js
+++ __tests__/BonusNumberTest.js
@@ -3,6 +3,7 @@ import { validateBonusNumber } from '../src/util/Validation.js';
 describe('보너스번호 테스트', () => {
   const MIN = 1;
   const MAX = 45;
+  const winnerNumbers = [1, 2, 3, 4, 5, 6];
 
   const randomNumber = (max) => {
     return Math.floor(Math.random() * max) + 1;
@@ -10,7 +11,7 @@ describe('보너스번호 테스트', () => {
 
   test.each([randomNumber(MAX)])('올바른 보너스 번호 테스트', (input) => {
     expect(() => {
-      validateBonusNumber(input);
+      validateBonusNumber(input, winnerNumbers);
     }).not.toThrow();
   });
 
@@ -18,7 +19,7 @@ describe('보너스번호 테스트', () => {
     '보너스 번호가 숫자형태가 아닌 경우 테스트',
     (input) => {
       expect(() => {
-        validateBonusNumber(input);
+        validateBonusNumber(input, winnerNumbers);
       }).toThrow();
     }
   );
@@ -27,8 +28,16 @@ describe('보너스번호 테스트', () => {
     '보너스 번호 숫자 범위를 벗어난 경우 테스트',
     (input) => {
       expect(() => {
-        validateBonusNumber(input);
+        validateBonusNumber(input, winnerNumbers);
       }).toThrow();
     }
   );
+
+  test('보너스 번호와 당첨번호 중복 테스트', () => {
+    const bonusNumber = '6';
+
+    expect(() => {
+      validateBonusNumber(bonusNumber, winnerNumbers);
+    }).toThrow();
+  });
 });
