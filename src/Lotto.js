87a656013235f5916d4d5104d0fc29e18dd479b5
백현영 <116708517+DOKIDOKI-b@users.noreply.github.com>
2023-12-03 13:56:20 +0900

    feat : 로또번호 관리 클래스

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,18 +1,44 @@
+import paramType from './lib/paramType/src/paramType.js';
+import AppError from './errors/AppError.js';
+
+const SEPERATE_SYMBOL = ',';
+
 class Lotto {
   #numbers;
 
-  constructor(numbers) {
+  constructor(numbers, _ = paramType(numbers, Array)) {
     this.#validate(numbers);
     this.#numbers = numbers;
   }
 
-  #validate(numbers) {
+  getNumbers() {
+    return [...this.#numbers];
+  }
+
+  #validate(numbers, _ = paramType(numbers, Array)) {
+    this.#checkLength(numbers);
+    this.#checkDuplicate(numbers);
+    this.#checkRange(numbers);
+  }
+
+  #checkLength(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new AppError('로또 번호는 6개여야 합니다.');
     }
   }
 
-  // TODO: 추가 기능 구현
+  #checkDuplicate(numbers) {
+    const set = new Set(numbers);
+    if (set.size !== numbers.length) {
+      throw new AppError('로또 번호에 중복된 숫자가 있습니다.');
+    }
+  }
+
+  #checkRange(numbers) {
+    if (!numbers.every((number) => number >= 1 && number <= 45)) {
+      throw new AppError('로또 번호는 1부터 45 사이의 숫자여야 합니다.');
+    }
+  }
 }
 
 export default Lotto;
