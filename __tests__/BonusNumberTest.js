251a4261ea0d0d39707d1e50f732d2df73bb6289
rlaclghks123 <rlaclghks123@naver.com>
2023-12-03 16:12:19 +0900

    test: 보너스번호 숫자범위 테스트

--- __tests__/BonusNumberTest.js
+++ __tests__/BonusNumberTest.js
@@ -1,18 +1,33 @@
 import { validateBonusNumber } from '../src/util/Validation.js';
 
 describe('보너스번호 테스트', () => {
-  test('보너스번호가 숫자형태인 경우  테스트', () => {
+  const MIN = 1;
+  const MAX = 45;
+
+  const randomNumber = (max) => {
+    return Math.floor(Math.random() * max) + 1;
+  };
+
+  test.each([randomNumber(MAX)])('올바른 보너스 번호 테스트', (input) => {
     expect(() => {
-      const input = '1';
       validateBonusNumber(input);
-    }).not.toThrow('[ERROR]');
+    }).not.toThrow();
   });
 
   test.each([[''], , ['     '], ['O'], ['1e3']])(
     '보너스 번호가 숫자형태가 아닌 경우 테스트',
     (input) => {
       expect(() => {
-        validateWinnerNumbers(input);
+        validateBonusNumber(input);
+      }).toThrow();
+    }
+  );
+
+  test.each([[`${MIN - 1}`], [`${MAX + 1}`]])(
+    '보너스 번호 숫자 범위를 벗어난 경우 테스트',
+    (input) => {
+      expect(() => {
+        validateBonusNumber(input);
       }).toThrow();
     }
   );
