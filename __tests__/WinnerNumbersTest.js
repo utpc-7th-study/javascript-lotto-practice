0f91e2112c602ae04b53adc2d33dbc235e57af5e
rlaclghks123 <rlaclghks123@naver.com>
2023-12-03 15:47:40 +0900

    test: 당첨번호 숫자형태 테스트

--- __tests__/WinnerNumbersTest.js
+++ __tests__/WinnerNumbersTest.js
@@ -14,4 +14,13 @@ describe('당첨번호 테스트', () => {
       validateWinnerNumbers(input);
     }).not.toThrow('[ERROR]');
   });
+
+  test.each([['1,2,3,4,5, '], ['1,2,3,4,5,q'], ['1,2,3,4,5,O'], ['1,2,3,4,5,   9    ']])(
+    '콤마(,)를 기준으로 숫자형태가 아닌 경우 테스트',
+    (input) => {
+      expect(() => {
+        validateWinnerNumbers(input);
+      }).toThrow();
+    }
+  );
 });
