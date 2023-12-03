6b7617fd7aa4241878fa8e83fef0365d5b8aeddd
rlaclghks123 <rlaclghks123@naver.com>
2023-12-03 13:45:47 +0900

    feat: 유효성 검사 구입금액 1,000원단위로 나눠지지 않는 경우 예외처리

--- src/domain/LottoGame.js
+++ src/domain/LottoGame.js
@@ -5,11 +5,18 @@ class LottoGame {
 
   #validateLottoPrice(lottoPrice) {
     this.#validateNumberType(lottoPrice);
+    this.#validateCurrencyUnit(lottoPrice);
   }
 
   #validateNumberType(input) {
     if (!/^[0-9]+$/.test(input)) {
       throw new Error('[ERROR] 로또 금액은 숫자형태로만 입력이 가능합니다.');
+    }
+  }
+
+  #validateCurrencyUnit(input) {
+    if (Number(input) % 1000 !== 0) {
+      throw new Error('[ERROR] 로또 금액은 1000원 단위로만 구입 가능합니다.');
     }
   }
 }
