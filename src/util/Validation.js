dd01712167a4c63c70dfd35629183f647d7b2265
rlaclghks123 <rlaclghks123@naver.com>
2023-12-03 15:45:08 +0900

    feat: 유효성검사 - 당첨번호 숫자형태 유효성검사

--- src/util/Validation.js
+++ src/util/Validation.js
@@ -1,9 +1,19 @@
 export const validateWinnerNumbers = (winnerNumbers) => {
-  validateMinimumLengthLimit(winnerNumbers);
+  const splitedNumbers = winnerNumbers.split(',');
+  validateMinimumLengthLimit(splitedNumbers);
+  validateNumberType(splitedNumbers);
 };
 
-const validateMinimumLengthLimit = (winnerNumbers) => {
-  if (winnerNumbers.split(',').length !== 6) {
-    throw new Error('[ERROR] 당첨번호는 쉼표(,)를 기준으로 6자리를 입력해주세요.');
+const validateMinimumLengthLimit = (splitedNumbers) => {
+  if (splitedNumbers.length !== 6) {
+    throw new Error('[ERROR] 당첨 번호는 쉼표(,)를 기준으로 6자리를 입력해주세요.');
+  }
+};
+
+const validateNumberType = (splitedNumbers) => {
+  const isValid = splitedNumbers.every((number) => /^[0-9]+$/.test(number));
+
+  if (!isValid) {
+    throw new Error('[ERROR] 당첨 번호는 쉼표(,)를 기준으로 숫자 형태만 입력해주세요.');
   }
 };
