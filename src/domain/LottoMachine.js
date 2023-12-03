42c99d6adcbe51182b1cfa41f71d8b46570db977
BK <3031011@naver.com>
2023-12-03 14:04:09 +0900

    refactor: 구입 금액 range 검증 책임 컨트롤러에서 로또머신 클래스로 분리

--- src/domain/LottoMachine.js
+++ src/domain/LottoMachine.js
@@ -2,24 +2,31 @@ import { Random } from '@woowacourse/mission-utils';
 import LOTTO_WINNING_INFO from '../constant/lotto/winningInfo.js';
 import { MAX, MIN } from '../constant/lotto/range.js';
 import LOTTO_PRICE from '../constant/lotto/price.js';
+import LOTTO_ERROR_MESSAGE from '../constant/lotto/errorMessage.js';
 
 class LottoMachine {
   #lottos = [];
 
   constructor(buyAmount) {
-    for (let i = 0; i < buyAmount / LOTTO_PRICE; i += 1) {
-      this.#generateLotto();
-    }
+    this.#validateBuyAmount(buyAmount);
+    this.#generateLotto(buyAmount);
   }
 
-  #generateLotto() {
-    const numbers = Random.pickUniqueNumbersInRange(
-      MIN.LOTTO_NUMBER,
-      MAX.LOTTO_NUMBER,
-      LOTTO_WINNING_INFO.first.correct,
-    );
+  #validateBuyAmount(buyAmount) {
+    if (buyAmount < MIN.BUY_AMOUNT) throw new Error(LOTTO_ERROR_MESSAGE.LESS_THAN_MIN_PRICE);
+    if (buyAmount % LOTTO_PRICE !== 0) throw new Error(LOTTO_ERROR_MESSAGE.INVALID_PRICE);
+  }
 
-    this.#lottos.push(numbers);
+  #generateLotto(buyAmount) {
+    for (let i = 0; i < buyAmount / LOTTO_PRICE; i += 1) {
+      const numbers = Random.pickUniqueNumbersInRange(
+        MIN.LOTTO_NUMBER,
+        MAX.LOTTO_NUMBER,
+        LOTTO_WINNING_INFO.first.correct,
+      );
+
+      this.#lottos.push(numbers);
+    }
   }
 }
 
