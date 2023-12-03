1a134e6ff4aaf83b8ed2d55562bad13506f31b4b
BK <3031011@naver.com>
2023-12-03 12:53:19 +0900

    feat: InputView, OutputView 작성 완료

---
+++ src/constant/lotto/winningInfo.js
@@ -0,0 +1,25 @@
+const LOTTO_WINNING_INFO = Object.freeze({
+  first: {
+    correct: 6,
+    prize: 2000000000,
+  },
+  second: {
+    correct: 5,
+    bonus: true,
+    prize: 30000000,
+  },
+  third: {
+    correct: 5,
+    prize: 1500000,
+  },
+  fourth: {
+    correct: 4,
+    prize: 50000,
+  },
+  fifty: {
+    correct: 3,
+    prize: 5000,
+  },
+});
+
+export default LOTTO_WINNING_INFO;
---
+++ src/view/OutputView.js
@@ -0,0 +1,44 @@
+import { Console } from '@woowacourse/mission-utils';
+import LOTTO_WINNING_INFO from '../constant/lotto/winningInfo';
+
+const OutputView = {
+  numberFormat: new Intl.NumberFormat('ko-KR'),
+
+  addCommaToNumber: (number) => this.numberFormat.format(number),
+
+  boughtLottos(lottos) {
+    // lottos : [[],[]]
+    const message = [
+      `${lottos.length}개를 구매했습니다.`,
+      ...lottos.map((lottoNumbers) => [`${lottoNumbers.join(', ')}`]),
+    ];
+
+    this.print(message.join('\n'));
+  },
+
+  correctTemp(matchNumber, prize, matchCount) {
+    return `${matchNumber}개 일치 (${this.addCommaToNumber(prize)}원) - ${matchCount}개`;
+  },
+
+  lottoResult(lottoStatistics) {
+    const { first, second, third, fourth, fifth, roi } = lottoStatistics;
+    const message = [
+      '당첨 통계',
+      '---',
+      this.correctTemp(LOTTO_WINNING_INFO.fifty.correct, LOTTO_WINNING_INFO.fifty.prize, fifth),
+      this.correctTemp(LOTTO_WINNING_INFO.fourth.correct, LOTTO_WINNING_INFO.fourth.prize, fourth),
+      this.correctTemp(LOTTO_WINNING_INFO.third.correct, LOTTO_WINNING_INFO.third.prize, third),
+      this.correctTemp(LOTTO_WINNING_INFO.second.correct, LOTTO_WINNING_INFO.second.prize, second),
+      this.correctTemp(LOTTO_WINNING_INFO.first.correct, LOTTO_WINNING_INFO.first.prize, first),
+      `총 수익률은 ${roi}%입니다.`,
+    ];
+
+    this.print(message.join('\n'));
+  },
+
+  print(message) {
+    Console.pirnt(message);
+  },
+};
+
+export default OutputView;
