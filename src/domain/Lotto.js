4de4c62dc6079df30a760c0dd6a4972e1055aa9c
rlaclghks123 <rlaclghks123@naver.com>
2023-12-03 14:58:37 +0900

    feat: 유효성검사 - 6개의 숫자가 아닌 경우 예외처리

--- src/domain/Lotto.js
+++ src/domain/Lotto.js
@@ -7,6 +7,10 @@ class Lotto {
   }
 
   #validate(numbers) {
+    this.#validateLimitLottoLength(numbers);
+  }
+
+  #validateLimitLottoLength(numbers) {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
