9c9cafc3f8d0201d429997a32d4a16791e1c2a91
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2023-12-04 20:57:38 +0900

    feat: 로또 번호 오름차순 정렬하는 기능 구현

--- src/domains/Lotto.js
+++ src/domains/Lotto.js
@@ -3,13 +3,19 @@ class Lotto {
 
   constructor(numbers) {
     this.#validate(numbers);
-    this.#numbers = numbers;
+    this.#numbers = this.#sort(numbers);
   }
 
   #validate(numbers) {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+  }
+
+  #sort(numbers) {
+    return numbers.sort(function (a, b) {
+      return a - b;
+    });
   }
 
   countSameNumber(lotto) {
